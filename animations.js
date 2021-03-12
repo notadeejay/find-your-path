3a5ed5a26afea419f89abb844ddcb1bbfa2c8b2a
notadeejay <amyemansell@gmail.com>
2021-03-12 12:51:30 -0500

    fixed layout

--- animations.js
+++ animations.js
@@ -25,7 +25,7 @@ function animateAll() {
 }
 
 function replaceHTML() {
- $('.question__text tw-link').html(function (i, html) {
+ $('.question__text').html(function (i, html) {
      var chars = $.trim(html).split(" ");
      return '<span class="letter">' + chars.join('</span> <span class="letter">') + '</span>';
     });
@@ -33,5 +33,29 @@ function replaceHTML() {
 }
 
 function startAnimations() {
-   var callFunc = setTimeout(function() {replaceHTML()}, 1000)
+   var callFunc = setTimeout(function() {replaceHTML()}, 500)
+}
+
+function popUpLetters() {
+    console.log('here')
+    anime({
+        loop: false,
+        targets: '.quiz-section .letter',   
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 100 * (i+1)
+      })
+
+    setTimeout(function() {$('.quiz-section').css('opacity', 1) ;}, 700)
+}
+
+function animateSection() {
+setTimeout(function() {
+    $('.quiz-section').html(function (i, html) {
+        var chars = $.trim(html).split("");
+        return '<span class="letter">' + chars.join('</span><span class="letter">') + '</span>';
+    });}, 500)
+    
+    setTimeout(function() {popUpLetters();}, 500)
 }
