0fc3b29b41aa85cbb196ef7e58f869f9dab51772
notadeejay <amyemansell@gmail.com>
2021-03-12 10:38:56 -0500

    fixed animation glitch

--- animations.js
+++ animations.js
@@ -1,35 +1,37 @@
 
 function animateAll() {
-
-    anime({
+      anime({
         loop: false,
-        targets: '.this .question__text .letter', 
-        translateX: [40,0],
+        targets: '.this .question__text .letter',   
+        translateY: [100,0],
         translateZ: 0,
         opacity: [0,1],
         easing: "easeOutExpo",
-        duration: 1200,
+        duration: 1400,
         delay: (el, i) => 700 + 30 * i
       })
-    anime({
+      anime({
         loop: false,
-        targets: '.that .question__text .letter', 
-        translateX: [40,0],
+        targets: '.that .question__text .letter',   
+        translateY: [100,0],
         translateZ: 0,
         opacity: [0,1],
         easing: "easeOutExpo",
-        duration: 1200,
+        duration: 1400,
         delay: (el, i) => 700 + 30 * i
       })
+    
+    setTimeout(function() {$('.question__text').css('opacity', 1);}, 700)
 }
 
 function replaceHTML() {
-
  $('.question__text tw-link').html(function (i, html) {
      var chars = $.trim(html).split(" ");
      return '<span class="letter">' + chars.join('</span> <span class="letter">') + '</span>';
     });
-
     animateAll()
 }
 
+function startAnimations() {
+   var callFunc = setTimeout(function() {replaceHTML()}, 1000)
+}
