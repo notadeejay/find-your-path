e14efdabeb3b350aff5f0787b097dcc4faead99f
notadeejay <amyemansell@gmail.com>
2021-03-11 20:36:06 -0500

    animations

---
+++ animations.js
@@ -0,0 +1,33 @@
+
+function animateAll() {
+    anime({
+        loop: false,
+        targets: '.this .question__text .letter', 
+        translateX: [40,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+      })
+    anime({
+        loop: false,
+        targets: '.that .question__text .letter', 
+        translateX: [40,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+      })
+}
+
+function replaceHTML() {
+ $('.question__text tw-link').html(function (i, html) {
+     var chars = $.trim(html).split(" ");
+     return '<span class="letter">' + chars.join('</span> <span class="letter">') + '</span>';
+    });
+
+    animateAll()
+}
+
