4326a3b20fa386fe144d5a46fd85fe62708090f3
notadeejay <amyemansell@gmail.com>
2021-04-05 10:03:57 -0400

    changed facebook share

--- sharing.js
+++ sharing.js
@@ -1,7 +1,7 @@
 
 function setLinks (result) {
     const tweetString = `I took the @Pathstream Find Your Path Quiz and got ${result}!`
-    const tweetUrlString = `http://twitter.com/share?url=http%3A%2F%2Fpathstream.com%2Fcampaigns%2Ffind-your-path&via=Pathstream&text=${tweetString.replace(/ /g, "%20")}`
+    const tweetUrlString = `http://twitter.com/share?url=http%3A%2F%2Fhttps://find-your-path.netlify.app/%2Fcampaigns%2Ffind-your-path&via=Pathstream&text=${tweetString.replace(/ /g, "%20")}`
     const aTwitter = document.getElementsByClassName('twitter-share')
     aTwitter[1].href = tweetUrlString 
     console.log(aTwitter)
@@ -12,7 +12,7 @@ function handleFacebookClick (result) {
     FB.ui({
         method: 'share',
         href: 'https://find-your-path.netlify.app/',
-        quote: `I got ${result} - how cool!`
+        quote: `I got ${result}!`
     }, function(response){
         if (response) {
             console.log('Facebook post published.');
